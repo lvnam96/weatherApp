8949351489bbc88fa863d98d18e6b8dfccc23dde
lvnam96 <lvnam96@gmail.com>
2017-05-06 20:00:15 +0700

    Revert "Update weatherApp.js"
    This reverts commit 1bf7cd8f3ac7bb18c75e23550fa686f8e0087193.
    
    # Conflicts:
    #	weatherApp.js

--- weatherApp.js
+++ weatherApp.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
   (function gettingJSON() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var userGPSURL = 'http:\/\/api.openweathermap.org\/data\/2.5\/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&appid=b50110aef08e2abcca1b47a1932b9e13';
+        var userGPSURL = 'https:\/\/crossorigin.me\/http:\/\/api.openweathermap.org\/data\/2.5\/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&appid=b50110aef08e2abcca1b47a1932b9e13';
         var request = new XMLHttpRequest();
         request.open('GET', userGPSURL, true);
         request.onload = function() {
